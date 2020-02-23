afedbab74f6af1f9588ee9b8ef5b1d0ce0dd14f0
Matt Loria <loria.matt@gmail.com>
2020-02-22 22:42:24 -0500

    Updates

---
+++ precache-manifest.211c46dcd19574f4186e80f3aba802e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e541fc8b5b79b9be77e5e5fb159e1598",
+    "url": "/ready-bready/index.html"
+  },
+  {
+    "revision": "0e8aceb88313f4478acf",
+    "url": "/ready-bready/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7b6ed5cf808a18c71554",
+    "url": "/ready-bready/static/js/2.76f14b84.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ready-bready/static/js/2.76f14b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8aceb88313f4478acf",
+    "url": "/ready-bready/static/js/main.7414852c.chunk.js"
+  },
+  {
+    "revision": "3a21ceb08aaac53bdf1b",
+    "url": "/ready-bready/static/js/runtime-main.8edcb53a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ready-bready/static/media/logo.5d5d9eef.svg"
+  }
+]);
